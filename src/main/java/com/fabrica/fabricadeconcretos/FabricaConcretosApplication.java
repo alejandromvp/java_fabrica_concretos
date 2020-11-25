194a8d405b17d8b43054b126427d9982f58903b0
alejandromvp <alejandro18maestro@gmail.com>
2020-11-25 20:16:16 -0300

    primer commit proyecto java spring

---
+++ src/main/java/com/fabrica/fabricadeconcretos/FabricaConcretosApplication.java
@@ -0,0 +1,13 @@
+package com.fabrica.fabricadeconcretos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricaConcretosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricaConcretosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabrica/fabricadeconcretos/FabricaConcretosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrica.fabricadeconcretos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricaConcretosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
